3b71bba64540395fd390a967df3771a6aabb2647
la-ris-sa <carvalh06.la@gmail.com>
2023-03-20 00:13:26 +0900

    :\)

--- www/components/motor.js
+++ www/components/motor.js
@@ -1,5 +1,13 @@
-// This is a JavaScript file
+/*function olamundo(){
+    var nome
+    nome = document.getElementById('nomeusuario').value
+    alert ("Olá " +nome)
+}*/
 
-function olamundo(){
-    alert('Olá Mundo!');
+function calcular() {
+   let valor1 = document.getElementById("valor1").value;
+   let valor2 = document.getElementById("valor2").value;
+   let valor3 = document.getElementById("valor3").value;
+   let resultado = (valor3 * valor2) / valor1;
+   document.getElementById("resultado").value = resultado;
 }
